783a5920da1a71a3ff256ca345b0363ea1523e78
Willis Aybar <willisaybar@gmail.com>
2020-02-03 23:25:15 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,8 +1,26 @@
 def third_challenge
   epic_tragedy = {
-   :montague => {
+    :montague => {
       :patriarch => {name: "Lord Montague", age: "53"},
       :matriarch => {name: "Lady Montague", age: "54"},
+      :hero => {name: "Romeo", age: "15", status: "alive"},
+      :hero_friends => []
+   },
+   :capulet => {
+      :patriarch => {name: "Lord Capulet", age: "50"},
+      :matriarch => {name: "Lady Capulet", age: "51"},
+      :heroine => {name: "Juliet", age: "15", status: "alive"},
+      :heroine_friends => []
+   }
+}
+ 
+
+  
+end
+
+
+def third_challenge
+  epic_tragedy = {:montague => {:other => {4},:matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => []
    },
